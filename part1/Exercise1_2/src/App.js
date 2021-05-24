8d8e95c0accc2fd55aa209c4f82c55e4acd4e95b
tarasny <>
2021-05-24 18:49:48 +0100

    Exercise1_2 completed

---
+++ part1/Exercise1_2/src/App.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const Part = (props) => {
+  return (
+  <>
+    <p>{props.part} has {props.exercises} exercises</p>
+  </>
+  )
+}
+const App = () => {
+  const course = 'Half stack applicatoin development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  
+  return (
+  /*  <div>
+      <h1>{course}</h1>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </div> */
+    <div>
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
+    </div>
+  )
+}
+
+export default App;
