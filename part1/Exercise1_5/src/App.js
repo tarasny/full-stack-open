caab778405391af9c1b22bfb8a2b83a7293ad520
tarasny <>
2021-05-27 19:45:59 +0100

    Completed exercise 1_5

--- part1/Exercise1_5/src/App.js
+++ part1/Exercise1_5/src/App.js
@@ -1,31 +1,56 @@
 import React from 'react'
 
-const Part = (props) => {
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.name}</h1>
+    </>
+  )
+}
+
+const Content = (props) => {
   return (
   <>
-    <p>{props.part} has {props.exercises} exercises</p>
+    {props.parts.map((part) => 
+      <p>{part.name} has {part.exercises} exercises</p>
+    )}
   </>
   )
 }
+
+const Total = (props) => {
+  var total = 0
+  return (
+    <>
+      <p hidden> {props.parts.map((part) => 
+        total += part.exercises)}</p>
+      <p>Number of exercises {total}</p>
+    </>
+  )
+}
 const App = () => {
-  const course = 'Half stack applicatoin development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
+  const course = {
+    name: 'Half stack application development',
+    parts: [
+      {
+      name: 'Fundamentals of React',
+      exercises: 10
+      },
+      {
+      name: 'Using props to pass data',
+      exercises: 7
+      },
+      {
+      name: 'State of a component',
+      exercises: 14
+      }
+    ]
   }
   return (
     <div>
-      {parts.map((part,exercises) =>
-        <p>{part} has {exercises} exercises</p>
-      )}
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
