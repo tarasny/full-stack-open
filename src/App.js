290d6d555b71efa98c7e3565b020e3fa9101e2ed
tarasny <>
2021-06-25 18:40:01 +0100

    Completed Exercise2_7

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
-import React from 'react'
-import Note from './components/Note'
+import React, { useState } from 'react'
 
-const App = ({ notes }) => {
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const addNew = (event) => {
+    event.preventDefault()
+    let isNew = true
+    const nameObj = {
+      name: newName,
+    }
+    persons.map(person => newName === person.name ? isNew = false : '')
+    isNew ? setPersons(persons.concat(nameObj)) : alert(`${nameObj.name} is already in the Phonebook`)
+    setNewName('')
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
   return (
     <div>
-      <h1>Notes</h1>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName}
+                       onChange={handleNoteChange}
+                />
+        </div>
+        <div>
+          <button type="submit" onClick={addNew}>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
       <ul>
-        {notes.map(note => 
-            <Note key={note.id} note={note} />
-        )}
+        {persons.map(person =>
+          <p key={person.name}>{person.name}</p>) 
+        }
       </ul>
     </div>
   )
